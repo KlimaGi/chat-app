2b8cfb9de6aa3211e863a99a1ff07b3ecc505ce6
Giedre Klimaite <giedre.klima@gmail.com>
2021-10-05 18:42:18 +0300

    socket.io rooms

--- src/index.js
+++ src/index.js
@@ -20,11 +20,16 @@ app.use(express.static(publicDirectoryPath));
 io.on("connection", (socket) => {
   console.log("New WebSocket connection");
 
-  //to single conected user
-  socket.emit("message", generateMessage("Welcome!"));
+  socket.on("join", ({ username, room }) => {
+    socket.join(room);
 
-  //everybody, except this user
-  socket.broadcast.emit("message", generateMessage("A new user has joined!"));
+    //to single conected user
+    socket.emit("message", generateMessage("Welcome!"));
+    //everybody, except this user in specific room
+    socket.broadcast
+      .to(room)
+      .emit("message", generateMessage(`${username} has joined!`));
+  });
 
   socket.on("sendMessage", (message, callback) => {
     const filter = new Filter();
@@ -34,7 +39,7 @@ io.on("connection", (socket) => {
     }
 
     //to all conected users
-    io.emit("message", generateMessage(message));
+    io.to("Center City").emit("message", generateMessage(message));
     callback();
   });
 
