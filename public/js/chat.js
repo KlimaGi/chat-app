6c50a7a680912aa899860cd39e5faa1d2dc312d9
Giedre Klimaite <giedre.klima@gmail.com>
2021-10-05 13:36:59 +0300

    styling the chat app

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,7 +17,7 @@ socket.on("message", (message) => {
   console.log(message);
   const html = Mustache.render(messageTemplate, {
     message: message.text,
-    createdAt: moment(message.createdAt).format("HH:mm (a)"),
+    createdAt: moment(message.createdAt).format("HH:mm"),
   });
   $messages.insertAdjacentHTML("beforeend", html);
 });
@@ -25,7 +25,7 @@ socket.on("message", (message) => {
 socket.on("locationMessage", (message) => {
   const html = Mustache.render(locationMessageTemplate, {
     url: message.url,
-    createdAt: moment(message.createdAt).format("HH:mm (a)"),
+    createdAt: moment(message.createdAt).format("HH:mm"),
   });
   $messages.insertAdjacentHTML("beforeend", html);
 });
