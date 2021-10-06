7abf2317cfed8e7ea29bad15482a707556ec339e
Giedre Klimaite <giedre.klima@gmail.com>
2021-10-06 17:59:17 +0300

    rendering user list

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,6 +12,7 @@ const messageTemplate = document.querySelector("#message-template").innerHTML;
 const locationMessageTemplate = document.querySelector(
   "#location-message-template"
 ).innerHTML;
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
 
 // Options
 const { username, room } = Qs.parse(location.search, {
@@ -35,6 +36,14 @@ socket.on("locationMessage", (message) => {
     createdAt: moment(message.createdAt).format("HH:mm"),
   });
   $messages.insertAdjacentHTML("beforeend", html);
+});
+
+socket.on("roomData", ({ room, users }) => {
+  const html = Mustache.render(sidebarTemplate, {
+    room,
+    users,
+  });
+  document.querySelector("#sidebar").innerHTML = html;
 });
 
 $messageForm.addEventListener("submit", (e) => {
