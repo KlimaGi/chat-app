307e910eaa18a095da50e8749829324e803ae950
Giedre Klimaite <giedre.klima@gmail.com>
2021-10-06 15:30:00 +0300

    sending messages to rooms

--- src/index.js
+++ src/index.js
@@ -36,16 +36,20 @@ io.on("connection", (socket) => {
     socket.join(user.room);
 
     //to single conected user
-    socket.emit("message", generateMessage("Welcome!"));
+    socket.emit("message", generateMessage("Admin", "Welcome!"));
     //everybody, except this user in specific room
     socket.broadcast
       .to(user.room)
-      .emit("message", generateMessage(`${user.username} has joined!`));
+      .emit(
+        "message",
+        generateMessage("Admin", `${user.username} has joined!`)
+      );
 
     callback();
   });
 
   socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
     const filter = new Filter();
 
     if (filter.isProfane(message)) {
@@ -53,14 +57,16 @@ io.on("connection", (socket) => {
     }
 
     //to all conected users
-    io.to("Center City").emit("message", generateMessage(message));
+    io.to(user.room).emit("message", generateMessage(user.username, message));
     callback();
   });
 
   socket.on("sendLocation", (coords, callback) => {
-    io.emit(
+    const user = getUser(socket.id);
+    io.to(user.room).emit(
       "locationMessage",
       generateLocationMessage(
+        user.username,
         `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
       )
     );
@@ -73,7 +79,7 @@ io.on("connection", (socket) => {
     if (user) {
       io.to(user.room).emit(
         "message",
-        generateMessage(`${user.username} has left!`)
+        generateMessage("Admin", `${user.username} has left!`)
       );
     }
   });
--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,12 +1,16 @@
-const generateMessage = (text) => {
+const e = require("express");
+
+const generateMessage = (username, text) => {
   return {
+    username,
     text,
     createdAt: new Date().getTime(),
   };
 };
 
-const generateLocationMessage = (url) => {
+const generateLocationMessage = (username, url) => {
   return {
+    username,
     url,
     createdAt: new Date().getTime(),
   };
